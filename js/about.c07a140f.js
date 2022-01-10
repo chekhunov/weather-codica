71623fb03faf520bd95457aa3c38299dfc48e5cd
Raday <scdesignk@gmail.com>
2022-01-10 10:26:16 +0200

    feat: new deploy

---
+++ js/about.c07a140f.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{a1d1:function(n,a,t){"use strict";t.r(a);var e=t("7a23"),u={class:"about"},c=Object(e["h"])("h1",null,"This is an about page",-1),o=[c];function s(n,a){return Object(e["u"])(),Object(e["g"])("div",u,o)}var b=t("6b0d"),i=t.n(b);const r={},d=i()(r,[["render",s]]);a["default"]=d}}]);
